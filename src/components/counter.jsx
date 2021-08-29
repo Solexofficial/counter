158cd0acc220a213538f75ff768071a1d1bb8f8f
Mark Pokrovsky <solexofficial@yandex.ru>
2021-08-29 12:55:32 +0300

    feat: single source of strength

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,41 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 
-const Counter = () => {
-	const [count, setCount] = useState(0);
-	const tags = ["tag1", "tag2", "tag3"];
-
-	const formCount = () => {
-		return count === 0 ? "Ноль" : count;
+const Counter = props => {
+	const formValue = () => {
+		return props.value === 0 ? "Ноль" : props.value;
 	};
 	const getBadgeClasses = () => {
 		let classes = "badge m-2 bg-";
-		classes += count === 0 ? "danger" : "primary";
+		classes += props.value === 0 ? "danger" : "primary";
 		return classes;
-	};
-
-	// const renderTags = () => {
-	// 	if (tags.length === 0) return "Тегов не найдено";
-	// 	return tags.map(tag => <li key={tag}>{tag}</li>);
-	// };
-
-	const handleIncrement = productId => {
-		console.log(productId);
-		setCount(count + 1);
-	};
-
-	const handleDecrement = productId => {
-		if (count === 0) return;
-		setCount(count - 1);
 	};
 
 	return (
 		<>
-			<span className={getBadgeClasses()}>{formCount()}</span>{" "}
-			<button onClick={() => handleIncrement({ id: 1 })} className='btn btn-secondary btn-sm'>
+			<h4>{props.name}</h4>
+			<span className={getBadgeClasses()}>{formValue()}</span>{" "}
+			<button onClick={() => props.onIncrement(props.id)} className='btn btn-secondary btn-sm'>
 				Increment
 			</button>
-			<button onClick={handleDecrement} className='btn btn-secondary btn-sm'>
+			<button onClick={() => props.onDecrement(props.id)} className='btn btn-secondary btn-sm'>
 				Decrement
+			</button>
+			<button className='btn btn-danger btn-sm m-2' onClick={() => props.onDelete(props.id)}>
+				Delete
 			</button>
 		</>
 	);
